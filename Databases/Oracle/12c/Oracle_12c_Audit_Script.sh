37fcc4720584f7f8c8395783c296fd4603c3f700
Benson Macharia <muchokibenson12@gmail.com>
2019-09-02 17:21:42 +0300

    Oracle Bash Script

--- Databases/Oracle/12c/Oracle_12c_Audit_Script.sh
+++ Databases/Oracle/12c/Oracle_12c_Audit_Script.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
+usage() { echo "Usage: $0 [-u <string>] [-p <string>] [-s <string>]" 1>&2; exit 1; }
+
+while getopts ":u:p:s:" o; do
+    case "${o}" in
+        u)
+            u=${OPTARG}
+            ;;
+        p)
+            p=${OPTARG}
+            ;;
+	s)
+            s=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${u}" ] || [ -z "${p}" ] || [ -z "${s}" ]; then
+    usage
+fi
+
+echo "Starting Oracle script"
+
 sqlplus -s /nolog << EOF
-CONNECT admin/password;
-
-whenever sqlerror exit sql.sqlcode;
-set echo off 
-set heading off
-
-@Oracle_12c_Audit_Script.sql
-
-exit;
-EOF 
+CONNECT ${u}/${p}@${s};
+  whenever sqlerror exit sql.sqlcode;
+  @Oracle_12c_Audit_Script.sql
+  exit;
+EOF
+ERR_CODE=$? # then $? is loaded with error received
+if [[ 0 != "${ERR_CODE}" ]] ; then
+  echo could not connect :\(
+else 
+  echo connection and script execution succeeded
+fi
